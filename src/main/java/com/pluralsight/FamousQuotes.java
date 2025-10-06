be45e12663f5d8e190fb655285d5ed978a6e4c59
Brittany Buchanan <bbuchanan0400@gmail.com>
2025-10-06 10:20:44 -0400

    - Initialized an array of 6 famous quotes.
    - Added try/catch that prints error message when selecting an out-of-bounds option.

---
+++ src/main/java/com/pluralsight/FamousQuotes.java
@@ -0,0 +1,32 @@
+package com.pluralsight;
+
+import java.util.Scanner;
+
+public class FamousQuotes {
+    public static void main(String[] args) {
+
+        try {
+            Scanner keyboard = new Scanner(System.in);
+
+            System.out.println("Select a number 1 - 10 to generate a quote: ");
+
+            int index = keyboard.nextInt();
+
+            String[] famousQuotes = {"\"I can accept failure, everyone fails at something. But I can't accept not trying.\" — Michael Jordan ",
+                    "\"Do not let making a living prevent you from making a life.\" — John Wooden",
+                    "“Always forgive your enemies; nothing annoys them so much.”\n" +
+                            "― Oscar Wilde",
+                    "“Yesterday is history, tomorrow is a mystery, today is a gift of God, which is why we call it the present.”\n" +
+                            "― Bill Keane", "“I have not failed. I've just found 10,000 ways that won't work.”\n" +
+                    "― Thomas A. Edison", "“A day without sunshine is like, you know, night.”\n" +
+                    "― Steve Martin"};
+
+
+            System.out.println(famousQuotes[index]);
+        } catch (Exception e) {
+
+            System.out.println("Sorry, that's out of range. Please choose a number 1 - 10");
+
+        }
+    }
+}
