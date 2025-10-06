843eba151445a2da2c462ab5bf9c186780999e60
Brittany Buchanan <bbuchanan0400@gmail.com>
2025-10-06 10:55:12 -0400

    - Added do/while to keep program running in case user wants additional quote

--- src/main/java/com/pluralsight/FamousQuotes.java
+++ src/main/java/com/pluralsight/FamousQuotes.java
@@ -5,28 +5,39 @@ import java.util.Scanner;
 public class FamousQuotes {
     public static void main(String[] args) {
 
-        try {
-            Scanner keyboard = new Scanner(System.in);
 
-            System.out.println("Select a number 1 - 10 to generate a quote: ");
+        Scanner keyboard = new Scanner(System.in);
 
-            int index = keyboard.nextInt();
+        String[] famousQuotes = {"\"I can accept failure, everyone fails at something. But I can't accept not trying.\" — Michael Jordan ",
+                "\"Do not let making a living prevent you from making a life.\" — John Wooden",
+                "“Always forgive your enemies; nothing annoys them so much.”\n" +
+                        "― Oscar Wilde",
+                "“Yesterday is history, tomorrow is a mystery, today is a gift of God, which is why we call it the present.”\n" +
+                        "― Bill Keane", "“I have not failed. I've just found 10,000 ways that won't work.”\n" +
+                "― Thomas A. Edison", "“A day without sunshine is like, you know, night.”\n" +
+                "― Steve Martin"};
 
-            String[] famousQuotes = {"\"I can accept failure, everyone fails at something. But I can't accept not trying.\" — Michael Jordan ",
-                    "\"Do not let making a living prevent you from making a life.\" — John Wooden",
-                    "“Always forgive your enemies; nothing annoys them so much.”\n" +
-                            "― Oscar Wilde",
-                    "“Yesterday is history, tomorrow is a mystery, today is a gift of God, which is why we call it the present.”\n" +
-                            "― Bill Keane", "“I have not failed. I've just found 10,000 ways that won't work.”\n" +
-                    "― Thomas A. Edison", "“A day without sunshine is like, you know, night.”\n" +
-                    "― Steve Martin"};
+        boolean continueProgram = true;
+
+        do {
+            try {
 
 
-            System.out.println(famousQuotes[index]);
-        } catch (Exception e) {
+                System.out.println("Select a number 1 - 10 to generate a quote: ");
 
-            System.out.println("Sorry, that's out of range. Please choose a number 1 - 10");
+                int index = keyboard.nextInt();
 
-        }
+
+                System.out.println(famousQuotes[index]);
+            } catch (Exception e) {
+
+                System.out.println("Sorry, that's out of range. Please choose a number 1 - 10");
+            }
+            keyboard.nextLine();
+            System.out.print("Would you like another quote? (Y/N)");
+            String choice = keyboard.nextLine();
+
+        } while (continueProgram == true);
+
     }
 }
